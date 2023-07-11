a0002d7a22505124ff3cf164e4314a9ac85483c3
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-11 15:51:18 +0330

    (chore): add template to movie-list-item

---
+++ src/components/MovieListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="movie-list-wrapper">
+    <div class="flex items-center h-[250px]">
+      <div class="movie-list-item">
+        <img src="../assets/images/01.jpg" class="movie-list-item-img" />
+        <span class="movie-list-item-title">Netflix Original</span>
+        <p class="movie-list-item-desc">Ready to watch? Enter your email to create or restart your membership.</p>
+        <button class="movie-list-item-button">Watch Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
