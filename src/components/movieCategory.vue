38d0981b3666ff1db0ca97cebb7045222b07d980
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-12 15:23:00 +0330

    (chore): add movie category vue

---
+++ src/components/movieCategory.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="font-poppins text-black dark:text-white font-semibold text-2xl mb-2 mt-10">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+<script setup>
+defineProps({ title: String });
+</script>
