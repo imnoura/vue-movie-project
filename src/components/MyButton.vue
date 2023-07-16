0c9323c2c794912228bd1e158f9e43557f37541a
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-16 15:43:36 +0330

    (style):implement button

---
+++ src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button :class="isLarge ? 'large-button' : 'small-button'">{{ title }}</button>
+</template>
+
+<script setup>
+defineProps({
+  isLarge: Boolean,
+  title: String,
+});
+</script>
+
+<style scoped>
+.small-button {
+  @apply px-2.5 py-2 bg-[#c70913] text-white cursor-pointer rounded-md outline-none border-none text-sm font-medium absolute bottom-[18%] left-6 opacity-0 transition-all duration-500 ease-in-out hover:scale-75;
+}
+
+.large-button {
+  @apply bg-[#f40612] text-white text-xl rounded-xl px-8 py-4 border-none outline-none mt-6 cursor-pointer transition-all duration-300 ease-in-out hover:scale-75;
+}
+</style>
