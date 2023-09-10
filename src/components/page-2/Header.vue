a9a8f3a5f9d0294aef09f6c27627dd261d20b3cb
imnoura <imnouramousavi@gmail.com>
2023-09-10 22:33:29 +0330

    feat: add header component

---
+++ src/components/page-2/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-6 bg-black fixed z-30 flex justify-between items-center p-8 text-white top-0">
+    <div class="grid items-center">
+      <img src="../assets/page-2-images/logo.png" class="w-28 h-10 cursor-pointer" />
+    </div>
+    <div class="flex items-center justify-end text-sm">
+      <div class="mr-2 text-white">
+        <span>UNLIMITED TV SHOWS & MOVIES</span>
+      </div>
+      <button class="bg-[#f40612] text-white ml-1 rounded-sm px-8 py-2 border-none outline-none cursor-pointer">JOIN NOW</button>
+      <button
+        class="bg-transparent border border-solid rounded-sm border-gray-400 text-white ml-2 px-5 py-1.5 outline-none cursor-pointer"
+      >
+        SIGN IN
+      </button>
+    </div>
+  </div>
+</template>
--- src/pages/page-two.vue
+++ src/pages/page-two.vue
@@ -1,20 +1,5 @@
 <template>
-  <header class="w-full h-6 bg-black fixed z-30 flex justify-between items-center p-8 text-white top-0">
-    <div class="grid items-center">
-      <img src="../assets/page-2-images/logo.png" class="w-28 h-10 cursor-pointer" />
-    </div>
-    <div class="flex items-center justify-end text-sm">
-      <div class="mr-2 text-white">
-        <span>UNLIMITED TV SHOWS & MOVIES</span>
-      </div>
-      <button class="bg-[#f40612] text-white ml-1 rounded-sm px-8 py-2 border-none outline-none cursor-pointer">JOIN NOW</button>
-      <button
-        class="bg-transparent border border-solid rounded-sm border-gray-400 text-white ml-2 px-5 py-1.5 outline-none cursor-pointer"
-      >
-        SIGN IN
-      </button>
-    </div>
-  </header>
+  <Header />
   <div class="mx-14 mt-10">
     <section class="image-wrapper">
       <div class="left-side-wrapper">
@@ -277,6 +262,7 @@
 </template>
 
 <script setup>
+import Header from "../components/page-2/Header.vue";
 import FooterSubtitle from "../components/page-2/FooterSubtitle.vue";
 import MovieCard from "../components/page-2/MovieCard.vue";
 import image03 from "../assets/page-2-images/03.jpg";
